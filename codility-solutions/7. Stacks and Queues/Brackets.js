c8ad59f7202c3b03f7edcce0e7effec2c9b02db1
ignorabilis <irina.yaroslavova@ignorabilis.com>
2018-09-22 14:11:41 +0000

    Add codility solutions

---
+++ codility-solutions/7. Stacks and Queues/Brackets.js
@@ -0,0 +1,28 @@
+// this code is ugly as hell, no time to refactor it
+function solution(S) {
+    let n = S.length,
+        st = [];
+
+    for(let i = 0; i < n; i ++){
+        let br = S[i],
+            br_p;
+
+        if('(' === br || '{' === br || '[' === br){
+            st.push(br);
+        }
+        else {
+            br_p = st.pop();
+
+            if(('(' === br_p && ')' === br)
+                || ('{' === br_p && '}' === br)
+                ||('[' === br_p && ']' === br)) {
+                // ps - this block here obviously does nothing
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+
+    return st.length > 0 ? 0 : 1;
+}
